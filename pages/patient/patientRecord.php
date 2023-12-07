36820e88d4ee051e9e9fe5d2b7b66268d06657f3
ShiroGeo <inocgeorgealfeser@gmail.com>
2023-12-07 21:05:26 +0800

    merge

--- pages/patient/patientRecord.php
+++ pages/patient/patientRecord.php
@@ -22,34 +22,32 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 
 <body>
-
-    <div class="d-flex" id="wrapper">
-
-        <!-- sidebar starts -->
-
-        <div class="background" id="sidebar-wrapper">
-            <div class="sidebar-heading text-center py-4 secondary-text fs-4 fw-bold text-uppercase border-bottom">
-                <i class="fas fa-hands-helping"></i>M.H COMPANION
-            </div>
-            <div class="list-group list-group-flush my-3">
-                <a href="PatientDashboard.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fab fa-hire-a-helper me-2"></i>Dashboard
-                </a>
-                <a href="PatientAppointment.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fas fa-table-list me-2"></i>Book Appointment
-                </a>
-                <a href="patientRecord.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fas fa-bookmark me-2"></i>Appointment Record
-                </a>
-                <a href="patientCinfo.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fas fa-info-circle me-2"></i>Councilor's Info
-                </a>
-                <a href="patientChat.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fas fa-comments me-2"></i>Chat Consultation
-                </a>
-                <a href="patientMedicalInfo.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fas fa-book-open me-2"></i>Medical Info
-                </a>
+    <div id="patient-user">
+        <div class="d-flex" id="wrapper">
+            <div class="background" id="sidebar-wrapper">
+                <div class="sidebar-heading text-center py-4 secondary-text fs-4 fw-bold text-uppercase border-bottom">
+                    <i class="fas fa-hands-helping"></i>M.H COMPANION
+                </div>
+                <div class="list-group list-group-flush my-3">
+                    <a href="PatientDashboard.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                        <i class="fas fa-gauge me-2"></i>Dashboard
+                    </a>
+                    <a href="PatientAppointment.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                        <i class="fas fa-house me-2"></i>Book Appointment
+                    </a>
+                    <a href="patientRecord.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                        <i class="fas fa-table-list me-2"></i>Appointment Record
+                    </a>
+                    <a href="patientCinfo.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                        <i class="fas fa-paw me-2"></i>Councilor's Info
+                    </a>
+                    <a href="patientChat.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                        <i class="fas fa-users me-2"></i>Chat Consultation
+                    </a>
+                    <a href="patientMedicalInfo.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+                        <i class="fas fa-users me-2"></i>Medical Info
+                    </a>
+                </div>
             </div>
             <div id="page-content-wrapper">
                 <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
@@ -133,9 +131,16 @@ if (!isset($_SESSION['user_id'])) {
             </div>
         </div>
     </div>
+<<<<<<< HEAD
     <script src="../../assets/js/axios.js"></script>
     <script src="../../assets/js/vue.3.js"></script>
     <script src="../../assets/js/patient.js"></script>
+=======
+
+
+
+
+>>>>>>> 724d5d4288a8ad9e982e3e9695bb8897e7ba0428
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
